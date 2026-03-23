066144f074444f7bc929b14af8d5f8bda403da8b
Zeromonster12 <152170025+Zeromonster12@users.noreply.github.com>
2026-03-23 16:43:18 +0100

    config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,18 +7,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
     host: '0.0.0.0',
     port: 5173,
+    allowedHosts: ['projectlinker.lznet.work'],
   },
 })
