a090ff5f2bca5519832ccc976f5d423aa0bd8743
Zeromonster12 <152170025+Zeromonster12@users.noreply.github.com>
2026-03-29 15:53:58 +0200

    fix

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function (): void {
     require base_path('routes/auth.php');
 
-    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    Route::middleware(['web', 'auth:sanctum'])->get('/user', function (Request $request) {
         return response()->json([
             'data' => $request->user(),
         ]);
